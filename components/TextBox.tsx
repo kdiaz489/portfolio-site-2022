5163c5aea7742367508c7899c653eeaf73ecbb85
Karen Diaz <kstefdiaz@gmail.com>
2022-09-02 20:49:34 -0700

    removed log

--- components/TextBox.tsx
+++ components/TextBox.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/TextBox.module.css';
 
 const TextBox: FunctionComponent = ({ children }) => {
   const { theme } = useTheme();
-  console.log('theme = ', theme);
+
   return (
     <div className={theme !== 'dark' ? styles.textbox : styles.textboxDark}>
       <span className={styles.textbox_heading}>{children}</span>
